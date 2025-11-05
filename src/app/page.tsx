98c4a4997155d0e151d015e443106d629000f2f8
Nahuel Caputto <nahuelmartinc@gmail.com>
2025-11-05 18:09:34 -0300

    ProductList with categories, footer and navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image";
+import ProductList from "./components/ProductList";
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <div className="">
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Featured Product" fill />
+      </div>
+      <ProductList />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
